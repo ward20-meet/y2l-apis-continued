4c3c1de5a383dcac9b05a8f867ea682b748705c2
myname21-meet <myname21@meet.mit.edu>
2019-12-12 12:00:50 -0500

    s

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 from flask import Flask, render_template, request
+import requests, json
 app = Flask(__name__)
+
+headers = {'Authorization': 'Key <YOUR_KEY_INSTEAD>'}
+
+api_url = "https://api.clarifai.com/v2/models/aaa03c23b3724a16a56b629203edc62c/outputs"
 
 @app.route('/')
 def home():
@@ -13,10 +18,13 @@ def study_image():
     # Your job now is to send this information to the Clarifai API
     # and read the result, make sure that you read and understand the
     # example we covered in the slides! 
+    data ={"inputs": [{"data": {"image": {"url": image_url}}}]}
 
-    # YOUR CODE HERE!
+	response = requests.post(api_url, headers=headers, data=json.dumps(data))
     
-    return render_template('home.html', results="No results yet :(")
+    return render_template('home.html', results=response.content)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
